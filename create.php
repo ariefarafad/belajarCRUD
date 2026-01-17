d3e51de38f448fc386f4c71b8de1d3273e0d086f
Arief Arafad <ariefarafad@gmail.com>
2026-01-17 11:43:10 +0700

    feat : buat Create

---
+++ create.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah Data Peserta</title>
+</head>
+<body>
+    <h1>Tambah Data Peserta</h1>
+    <form method="post">
+        <label>Nama : </label>
+        <input type="text" name="nama"> <br>
+        <label>Email : </label>
+        <input type="email" name="email"> <br>
+        <label>Usia : </label>
+        <input type="number" name="usia"> <br>
+        <button type="submit" name="tambah">Tambah</button> <br>
+        <a href="koneksi.php">Kembali</a>
+    </form>
+</body>
+</html>
